78b45d5f805e619a2d5c9bde1b2e9b9bfb02a76c
Shelly Jurbinsky <shellyjur@gmail.com>
2021-05-21 12:58:20 +0300

    Added properties to UnderMenu class

--- Ex04.Menus.Interfaces/MenuItem.cs
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -6,5 +6,27 @@ namespace Ex04.Menus.Interfaces
 {
     class MenuItem//buttom
     {
+        private string m_MenuName;
+
+        internal MenuItem(string i_MenuName)
+        {
+            m_MenuName = i_MenuName;
+        }
+
+        ////Properties:
+       public string MenuName
+        {
+            get
+            {
+                return m_MenuName;
+            }
+
+            set
+            {
+                m_MenuName = value;
+            }
+        }
+
+        
     }
 }
